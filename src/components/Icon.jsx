7810eff2f0a2bb9117794243c6b99fc02a61d6e3
enesakkose <ens.akkose@gmail.com>
2022-09-10 17:21:38 +0300

    fix

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,9 +1,10 @@
 import SVG from "react-inlinesvg";
 
+
 function Icon({ name, size = 16, ...props }) {
 	return (
 		<SVG
-			src={`../../public/icons/${name}.svg`}
+			src={`../icons/${name}.svg`}
 			width={size}
 			height={size}
 			{...props}
